01275b6cf22cf5c9f6f4102ae91c58c4da22e7ac
joaopnj <joaopnj@icloud.com>
2015-11-15 17:14:36 -0200

    Inclusão da classe Exceções
    Incluindo uma classe ExcecaoGenerica para o projeto, para fins de
    robustes.

---
+++ TI/TI/ExcecaoGenerica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TI
+{
+		public class ExcecaoGenerica : Exception
+		{
+			public string  erroGenerico{get;set;}
+
+			public ExcecaoGenerica() { }
+
+			public ExcecaoGenerica(string aErroGenerico)
+			{
+				this.erroGenerico = erroGenerico+" "+aErroGenerico;
+			}
+
+			public override string ToString()
+			{
+				return erroGenerico;
+			}
+		}
+}
+
+
